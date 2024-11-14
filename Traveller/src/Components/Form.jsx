2a1a491178c4036e1e2cc4530171b25eb89bfe9a
Omondi003 <joelomondi2002@gmail.com>
2024-11-14 11:30:22 +0300

    added usestate to the form

--- Traveller/src/Components/Form.jsx
+++ Traveller/src/Components/Form.jsx
@@ -1,16 +1,33 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { useState } from 'react'
 
 function Form() {
+      //State for managing the items both the items 
+      const [items,setItems]= useState({
 
+         selectedNumber: 1,
+         textInput: '',
+      })
+
+
+ 
     function handleSubmit(e){
+
+     console.log(items)
+     
      e.preventDefault()
     }
+
+
      return <form className='Addform' onSubmit={handleSubmit}>
        <h2>What do you want to pack</h2>
        <div className='options'>
  
-       <select>
+       <select
+        name='selectedNumber'
+        value={items.selectedNumber}
+       >
         {Array.from({length:20}, (_,i)=> i+1).map(
          (num)=> (
            <option value={num} key={num}>
@@ -20,8 +37,12 @@ function Form() {
         )}
        </select>
  
-       <input type='text'></input>
-       <button>Submit</button>
+       <input 
+         type='text'
+         value={items.textInput}
+
+        />
+               <button>Submit</button>
        </div>
         
      </form>
